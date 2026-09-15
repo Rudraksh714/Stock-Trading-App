261095f158efcd0fa17e2151b690953cc87ca87e
Rudraksh Mishra <rudraksh0702@gmail.com>
2026-09-15 11:30:50 +0530

    Fix signup authentication flow

--- backend/server/Routes/AuthRoute.js
+++ backend/server/Routes/AuthRoute.js
@@ -1,10 +1,25 @@
 const { Signup, Login, Logout } = require("../Controllers/AuthController");
 const router = require("express").Router();
 const { userVerification } = require("../Middlewares/AuthMiddleware");
+const User = require("../Models/UserModel");
 
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post("/", userVerification);
+
+router.post("/", userVerification, (req, res) => {
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        return res.json({ status: false });
+      }
+      return res.json({ status: true, user: user.username });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ status: false });
+    });
+});
+
 router.post("/logout", Logout);
 
 module.exports = router;
